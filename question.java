c2666bf07fa8fe053ea928a0f68a7a61ef9509c5
Kajol shah <106387777+Kajolshah29@users.noreply.github.com>
2024-06-09 18:39:12 +0530

    Create question.java

---
+++ question.java
@@ -0,0 +1,29 @@
+// "static void main" must be defined in a public class.
+public class Main {
+    static int CountOfOccurance(int[]arr,int x){
+        int count=0;
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]==x){
+                    count++;
+                }
+
+            }
+                        return count;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements: ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter x");
+       int x = sc.nextInt();
+
+     System.out.println("COUNT OF X: " + CountOfOccurance(arr, x));
+    }
+}
