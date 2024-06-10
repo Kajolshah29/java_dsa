6d7f34e246f51a15cd8a43d132961b70f126f600
Kajol shah <106387777+Kajolshah29@users.noreply.github.com>
2024-06-10 22:42:01 +0530

    Create repeatingnumber.java

---
+++ TargetSum-Problem/repeatingnumber.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+public class Main {
+    static int repeatingnumber(int[]arr){
+        int n=arr.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                   if(arr[i]==arr[j]){
+                       return arr[i];
+                   }
+            }
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[]arr=new int[n];
+        
+        System.out.println("enter " + n + " elements");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();   
+        }
+        System.out.println("Unique element :  " + repeatingnumber(arr) );
+    }
+}
