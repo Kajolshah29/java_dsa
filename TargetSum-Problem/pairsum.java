238b046ab5ab607fa62d6f9fa2eb81f9b509d969
Kajol shah <106387777+Kajolshah29@users.noreply.github.com>
2024-06-10 19:47:52 +0530

    Create pairsum.java

---
+++ TargetSum-Problem/pairsum.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    static int pairsum(int[]arr, int target){
+        int n=arr.length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]+arr[j]==target){
+                    ans++;
+                }
+            }
+
+        }
+                    return ans;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[]arr=new int[n];
+        
+        System.out.println("enter " + n + " elements");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            
+        }
+        System.out.println("enter the target sum");
+        int target=sc.nextInt();
+        
+        System.out.println("total number of pairs : " + pairsum(arr,target));
+        
+    }
+}
